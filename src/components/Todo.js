139ca8204b9aa2ecbc5aab3e1c1bc3ad53c5c23c
krishnagudladana <krishnagc108@gmail.com>
2023-09-05 23:36:55 +0530

    todo list

---
+++ src/components/Todo.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+import TodoForm from './TodoForm'
+
+function Todo() {
+
+  const [edit, setEdit] = useState({
+    id:null,
+    value : ''
+  })
+
+  
+  return (
+    <div>
+      <TodoForm />
+    </div>
+  )
+}
+
+export default Todo
